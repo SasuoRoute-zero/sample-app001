bfe7c1cdb2b0caa3f384dafea65a3eb300274dd0
RZ-Sasuo <sasuo@route-zero.com>
2021-05-17 02:44:01 +0900

    フロント実装

---
+++ src/app/products.ts
@@ -0,0 +1,53 @@
+export const products = [
+  {
+    coverImage:'.\\assets\\img\\phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'heading1',
+    heading2: 'heading2',
+    heading3: 'heading3',
+    headingtext1:'サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1',
+    headingtext2:'サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2',
+    headingtext3:'サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3'
+  },
+  {
+    coverImage:'.\\assets\\img\\phone-cover.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'heading1',
+    heading2: 'heading2',
+    heading3: 'heading3',
+    headingtext1:'サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1',
+    headingtext2:'サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2',
+    headingtext3:'サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3'
+
+  },
+  {
+    coverImage:'.\\assets\\img\\phone-cover.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heading1: 'heading1',
+    heading2: 'heading2',
+    heading3: 'heading3',
+    headingtext1:'サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1',
+    headingtext2:'サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2',
+    headingtext3:'サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3'
+
+  },
+  {
+    coverImage:'.\\assets\\img\\phone-cover.jpg',
+    name: 'Phone Special',
+    price: 999,
+    description: '',
+    heading1: 'heading1',
+    heading2: 'heading2',
+    heading3: 'heading3',
+    headingtext1:'サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1',
+    headingtext2:'サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2サンプルテキスト2',
+    headingtext3:'サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3サンプルテキスト3'
+
+  }
+];
